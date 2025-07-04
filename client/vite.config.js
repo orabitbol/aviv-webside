80ca8938a6ba568a630a4cebc1f2ac499263bf71
OrAbitbol <orabitbol@gmail.com>
2025-07-04 18:49:24 +0300

    ג

--- client/vite.config.js
+++ client/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
   server: {
     allowedHosts: true,
     proxy: {
-      '/api': process.env.VITE_API_URL || process.env.VITE_API_BASE_URL
+      '/api': process.env.VITE_API_URL
     }
   },
   resolve: {
