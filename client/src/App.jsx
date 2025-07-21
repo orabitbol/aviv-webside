0b2416fee3da1c4e5f5cb424bd413d1c0c3e3cf4
OrAbitbol <orabitbol@gmail.com>
2025-07-21 11:54:16 +0300

    add analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import Pages from "@/pages/index.jsx"
 import { Toaster } from "@/components/ui/toaster"
-
+import { Analytics } from "@vercel/analytics/next"
 function App() {
   return (
     <>
       <Pages />
       <Toaster />
+      <Analytics />
     </>
-  )
+  );
 }
 
 export default App 
